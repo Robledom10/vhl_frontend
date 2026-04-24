f9d9da91ea0a4c78950d675e049795f728621d7b
Sara Barrera <barrerasara2008@gmail.com>
2026-04-24 14:18:47 -0500

    Creacion de carpeta banner para la parte de registro

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { AuthFormComponent } from './components/app-auth-banner/app-auth-banner.component';
+import { LoginFormComponent } from './pages/login/components/login-form/login-form.component';
+import { RegisterFormComponent } from './pages/register/components/register-form/register-form.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    AuthFormComponent,
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     CommonModule,
