9574e6bfc60e4adc91ba8bade684353dce9d823f
miguel robledo <stmiguelangelgarciarobledo@gmail.com>
2026-06-16 12:51:43 -0500

    Despliegue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	apiUrl: 'https://api.example.com/api', // Replace with production API URL
+	apiUrl: 'https://crinkly-unifier-passive.ngrok-free.dev', // Replace with production API URL
 	chatbotUrl: 'https://chatbot.example.com', // Replace with production chatbot URL
 	googleClientId: 'REPLACE_WITH_PRODUCTION_GOOGLE_CLIENT_ID', // Set production Google Client ID
 };
